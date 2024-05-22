893c97f7b2d1f1cf0d35ac62b102746ed950b1b4
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-05-22 14:58:54 +0300

    FIX: About us page

---
+++ app/(home)/about/page.tsx
@@ -0,0 +1,42 @@
+export default function AboutPage() {
+  return (
+    <div className="px-6 lg:w-5/6 xl:w-4/6 py-10 mx-auto">
+      <div className="space-y-12 text-md md:text-lg text-gray-500 pb-4 md:pb-8">
+        <p>
+        Welcome to WellnessWise, your ultimate fitness companion on the journey to a healthier, happier you!
+        </p>
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            Workout Program
+          </h2>
+          <p>
+          Our customized workout programs are crafted to help you reach your fitness goals efficiently and effectively. 
+          Whether you aim to build muscle, boost endurance, or enhance overall fitness, our diverse selection of workouts provides a comprehensive and personalized exercise plan tailored to your needs and schedule.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            📊   Weight Analytics
+          </h2>
+          <p>
+            Track your weight loss or gain progress with our advanced weight analytics. 
+            Our intuitive weight tracking system allows you to monitor your weight changes over time, set achievable goals, and stay motivated on your fitness journey. 
+            With our weight analytics, you can visualize your progress, identify trends, and make informed decisions to optimize your health and fitness routine.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            🏋️‍♀️ Body Composition Analytics
+          </h2>
+          <p>
+            Achieve a better understanding of your body composition with our cutting-edge body composition analytics. 
+            Our advanced analytics provide detailed insights into your body fat percentage, muscle mass, bone density, and more, enabling you to make informed decisions to improve your health and fitness.
+            With our body composition analytics, you can track your progress, set realistic goals, and optimize your fitness routine for optimal results.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
